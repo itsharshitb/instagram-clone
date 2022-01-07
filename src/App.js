6595791c95ea8d13512ae06d35fc679874e0be60
Harshit <harshitbhatt972@gmail.com>
2022-01-08 01:05:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello let's build instagram clone @_@</h1>
+    </div>
+  );
+}
+
+export default App;
