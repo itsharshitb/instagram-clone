d85570e5f88054436bba6b041fd00a28308db1b9
Harshit <harshitbhatt972@gmail.com>
2022-01-13 19:54:40 +0530

    login modal added still customzing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,96 @@
 import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 import "./App.css";
 import Post from "./Post";
 import { db } from "./firebase";
+import { Input } from "@mui/material";
 
 function App() {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => {
-		setPosts(snapshot.docs.map(doc => doc.data()));
-	})
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
   }, []);
+
+  const signUp = (event) => {};
 
   return (
     <div className="app">
       {/* {header} */}
+      <div>
+        <Modal open={open} onClose={() => setOpen(false)}>
+          <Box sx={style}>
+            <form className="app_signup">
+              <center>
+                <img
+                  className="app_headerImage"
+                  src="https://www.pngjoy.com/pngm/80/1718089_instagram-black-logo-transparent-background-instagram-logo-hd.png"
+                  alt=""
+                />
+              </center>
+              <Input
+                placeholder="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Input
+                placeholder="email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button onClick={signUp}>Sign Up</Button>
+            </form>
+          </Box>
+        </Modal>
+      </div>
       <div className="">
         <img
           className="app_headerImage"
           src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F04%2FInstagram-Logo-2010-2013.png&f=1&nofb=1"
           alt=""
         />
+        <Button onClick={handleOpen}>Login</Button>
       </div>
       <h1>Hello let's build instagram clone @_@</h1>
       {/* {posts} */}
-      {posts.map((post) => (
+      {posts.map(({ id, post }) => (
         <Post
+          key={id}
           username={post.username}
           caption={post.caption}
           imageUrl={post.imageUrl}
