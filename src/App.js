868da821ff88c9e561161eace1dc7bb89c8b6722
Harshit <harshitbhatt972@gmail.com>
2022-01-13 23:59:44 +0530

    full user authentication done via firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import "./App.css";
 import Post from "./Post";
-import { db } from "./firebase";
+import { db, auth } from "./firebase";
 import { Input } from "@mui/material";
 
 function App() {
@@ -23,10 +23,26 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [openSignIn, setOpenSignIn] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [user, username]);
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -39,52 +55,107 @@ function App() {
     });
   }, []);
 
-  const signUp = (event) => {};
+  const signUp = (event) => {
+    event.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+    .signInWithEmailAndPassword(email,password)
+    .catch((error) => alert(error.message));
+
+    setOpenSignIn(false);
+  }
 
   return (
     <div className="app">
       {/* {header} */}
-      <div>
-        <Modal open={open} onClose={() => setOpen(false)}>
-          <Box sx={style}>
-            <form className="app_signup">
-              <center>
-                <img
-                  className="app_headerImage"
-                  src="https://www.pngjoy.com/pngm/80/1718089_instagram-black-logo-transparent-background-instagram-logo-hd.png"
-                  alt=""
-                />
-              </center>
-              <Input
-                placeholder="username"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box sx={style}>
+          <form className="app_signup">
+            <center>
+              <img
+                className="app_headerImage"
+                src="https://www.pngjoy.com/pngm/80/1718089_instagram-black-logo-transparent-background-instagram-logo-hd.png"
+                alt=""
               />
-              <Input
-                placeholder="email"
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>
+              Sign Up
+            </Button>
+          </form>
+        </Box>
+      </Modal>
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <Box sx={style}>
+          <form className="app_signup">
+            <center>
+              <img
+                className="app_headerImage"
+                src="https://www.pngjoy.com/pngm/80/1718089_instagram-black-logo-transparent-background-instagram-logo-hd.png"
+                alt=""
               />
-              <Input
-                placeholder="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button onClick={signUp}>Sign Up</Button>
-            </form>
-          </Box>
-        </Modal>
-      </div>
+            </center>
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signIn}>
+              Sign In
+            </Button>
+          </form>
+        </Box>
+      </Modal>
       <div className="">
         <img
           className="app_headerImage"
           src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F04%2FInstagram-Logo-2010-2013.png&f=1&nofb=1"
           alt=""
         />
-        <Button onClick={handleOpen}>Login</Button>
+        {user ? (
+          <Button onClick={() => auth.signOut()}>Logout</Button>
+        ) : (
+          <div className="app_loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
       <h1>Hello let's build instagram clone @_@</h1>
       {/* {posts} */}
